16b8d05f9bcfffbcbbfc2770dd872c5408375bd2
Jozsa Csongor <csongor.jozsa@emarsys.com>
2019-02-02 22:14:45 +0100

    Challenge 2: Fixed XOR

---
+++ lib/scoring.py
@@ -0,0 +1,38 @@
+from abc import ABC
+from sortedcontainers import SortedDict
+
+class ScoringStrategy(ABC):
+    def score(self, plaintext: str):
+        raise NotImplementedError()
+
+class WordScoringStrategy(ScoringStrategy):
+    def __init__(self, word_list_file: str):
+        with open(word_list_file) as words:
+            self._words = [line.lower().strip() for line in words]
+
+    def _possible_words(self, sentence: str) -> list:
+        return [sentence[word_start:word_end].lower() for word_start in range(len(sentence)-1) for word_end in range(word_start, len(sentence))]
+
+    def _is_valid_word(self, word: str) -> bool:
+        return word in self._words
+
+class WordLengthScoringStrategy(WordScoringStrategy):
+    def score(self, sentence: str) -> int:
+        words = self._possible_words(sentence)
+        return sum([len(word) for word in words if self._is_valid_word(word)])
+
+class WordCountScoringStrategy(WordScoringStrategy):
+    def score(self, sentence: str) -> int:
+        words = self._possible_words(sentence)
+        return len([word for word in words if self._is_valid_word(word)])
+
+class PlainTextGuesser():
+    def __init__(self, scoring_strategy: ScoringStrategy):
+        self._scoring_strategy = scoring_strategy
+
+    def most_likely_plaintext(self, candidates: list) -> dict:
+        ordered_plaintexts = SortedDict(((self._scoring_strategy.score(plaintext), (key, plaintext)) for key, plaintext in candidates.items()))
+
+        (_, (key,message)) = ordered_plaintexts.peekitem()
+
+        return {'key': key, 'message': message}
---
+++ lib/xor.py
@@ -0,0 +1,5 @@
+def sxorc(string: str, char: str) -> bytes:
+    return ''.join([chr(c1^char) for c1 in string])
+
+def sxors(string1: str, string2: str) -> bytes:
+    return b'%x' % (int(string1, 16) ^ int(string2, 16))
