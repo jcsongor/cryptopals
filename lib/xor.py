e8f93eacfe6a9fb6374cf5c16b9c70c7bfc229be
jcsongor <jozsa.csongor@gmail.com>
2019-02-02 22:30:47 +0100

    Fix code formatting

--- lib/xor.py
+++ lib/xor.py
@@ -1,5 +1,7 @@
+
 def sxorc(string: str, char: str) -> bytes:
-    return ''.join([chr(c1^char) for c1 in string])
+    return ''.join([chr(c1 ^ char) for c1 in string])
+
 
 def sxors(string1: str, string2: str) -> bytes:
     return b'%x' % (int(string1, 16) ^ int(string2, 16))
