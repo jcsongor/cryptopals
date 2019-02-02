80e4655e978ac4f86bd54fdf8f1ecfd7a05c45a5
Jozsa Csongor <csongor.jozsa@emarsys.com>
2019-02-02 22:16:00 +0100

    Challenge 3: Single-byte XOR cipher

---
+++ 3.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from binascii import unhexlify
+from lib.xor import sxorc
+from lib.scoring import PlainTextGuesser, ScoringStrategy, WordCountScoringStrategy, WordLengthScoringStrategy
+
+def decipher_with_every_key(ciphertext: str) -> dict:
+    return { chr(key): sxorc(ciphertext, key) for key in range(ord('a'), ord('z')) }
+
+def guess_plaintext(possible_plaintexts: list, scoring_strategy: ScoringStrategy) -> None:
+    guesser = PlainTextGuesser(scoring_strategy)
+
+    plaintext = guesser.most_likely_plaintext(possible_plaintexts)
+
+    assert plaintext['key'] == 'x'
+    assert plaintext['message'] == 'cOOKING\x00mc\x07S\x00LIKE\x00A\x00POUND\x00OF\x00BACON'
+
+ciphertext = unhexlify('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736')
+
+possible_plaintexts = decipher_with_every_key(ciphertext)
+
+guess_plaintext(possible_plaintexts, WordCountScoringStrategy('words.txt'))
+
+guess_plaintext(possible_plaintexts, WordLengthScoringStrategy('words.txt'))
