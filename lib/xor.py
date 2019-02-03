a3d7a532eb5975377078f4baa0980dbdca5879c7
jcsongor <jozsa.csongor@gmail.com>
2019-02-03 21:59:35 +0100

    Fix typehints

--- lib/xor.py
+++ lib/xor.py
@@ -1,7 +1,7 @@
 
-def sxorc(string: str, char: str) -> bytes:
+def sxorc(string: str, char: int) -> str:
     return ''.join([chr(c1 ^ char) for c1 in string])
 
 
-def sxors(string1: str, string2: str) -> bytes:
-    return b'%x' % (int(string1, 16) ^ int(string2, 16))
+def sxors(string1: str, string2: str) -> str:
+    return '%x' % (int(string1, 16) ^ int(string2, 16))
