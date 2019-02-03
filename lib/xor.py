e1b1ce4ca4f23e34af9652d6c7da57f343936398
jcsongor <jozsa.csongor@gmail.com>
2019-02-03 22:19:08 +0100

    Fix type

--- lib/xor.py
+++ lib/xor.py
@@ -1,6 +1,6 @@
 
 def sxorc(string: str, char: int) -> str:
-    return ''.join([chr(c1 ^ char) for c1 in string])
+    return ''.join([chr(int(c1) ^ char) for c1 in string])
 
 
 def sxors(string1: str, string2: str) -> str:
